da1ccc29951fb5231e56f065fc77346c7aa249b5
Frank Fazio <fjf.colourless@gmail.com>
2020-08-18 15:57:37 -0700

    Init

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,37 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { ReactiveFormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+//services
+import { RecipeService } from './services/recipe.service';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { HomeComponent } from './pages/home/home.component';
+import { EditRecipeComponent } from './pages/edit-recipe/edit-recipe.component';
+import { NewRecipeComponent } from './pages/new-recipe/new-recipe.component';
+import { ShowRecipeComponent } from './pages/show-recipe/show-recipe.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    HomeComponent,
+    EditRecipeComponent,
+    NewRecipeComponent,
+    ShowRecipeComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    ReactiveFormsModule
   ],
-  providers: [],
+  providers: [
+    RecipeService
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
